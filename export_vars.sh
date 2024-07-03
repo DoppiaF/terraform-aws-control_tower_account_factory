21b2362a47d7ae6152ddd57083e731718f202a88
FrancescoF <98francescofurini@gmail.com>
2024-07-03 17:44:30 +0200

    testing.9 problemi building_aggiunto script per settare var di ambiente su ambiente di build con i dati degli account

---
+++ export_vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Esporta le configurazioni come variabili d'ambiente
+export TF_VAR_log_archive_account_id="905418464727"
+export TF_VAR_audit_account_id="851725568356"
+export TF_VAR_aft_management_account_id="590183691381"
+export TF_VAR_ct_management_account_id="992382556812"
+
+# Stampa le variabili per conferma (opzionale)
+echo "Variabili d'ambiente configurate per Terraform."
